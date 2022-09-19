26a6c2f3dbebd99aaf727f2548996eafaaa69de3
feddericahu99 <federica.hu339@gmail.com>
2022-09-19 16:19:48 +0200

    fixed use App in home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
+
 class HomeController extends Controller
 {
     public function index() 
---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <h1> {{  }} </h1>
+            <h3> {{  }} </h3>
+            <h3> {{  }} </h3>
+            <h4> {{  }} </h4>
+            <h2> {{  }} </h2>
+        </div>
+    </div>
+@endsection
